31602411d1e7857e15bed858c1977f50aeb39ec7
Abidski <abijanahmed@gmail.com>
2026-06-09 20:08:41 -0400

    Remove unsused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import Header from "./components/Header.tsx";
 import Body from "./components/Body.tsx";
 import Footer from "./components/Footer.tsx";
